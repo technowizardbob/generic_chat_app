89bcab9372bf7f8c5443a04e6d1414e96402c9c4
Robert <Robert@TryingToScale.com>
2022-03-21 13:19:24 -0400

    Made GUI glade layout use grids.

--- lanchat/main.go
+++ lanchat/main.go
@@ -13,8 +13,8 @@ import (
 	"net"
 	"os"
 
+	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/glib"
-
 	"github.com/gotk3/gotk3/gtk"
 )
 
@@ -24,8 +24,8 @@ const gladeTemplate = `<?xml version="1.0" encoding="UTF-8"?>
   <requires lib="gtk+" version="3.20"/>
   <object class="GtkWindow" id="myPopup">
     <property name="name">LanChatApp</property>
-    <property name="width_request">590</property>
-    <property name="height_request">500</property>
+    <property name="width_request">600</property>
+    <property name="height_request">600</property>
     <property name="visible">True</property>
     <property name="can_focus">True</property>
     <property name="has_focus">True</property>
@@ -40,8 +40,6 @@ const gladeTemplate = `<?xml version="1.0" encoding="UTF-8"?>
     <property name="window_position">center</property>
     <property name="destroy_with_parent">True</property>
     <property name="icon_name">network-transmit-receive</property>
-    <property name="type_hint">utility</property>
-    <property name="decorated">False</property>
     <property name="deletable">False</property>
     <property name="gravity">north</property>
     <property name="has_resize_grip">True</property>
@@ -49,81 +47,117 @@ const gladeTemplate = `<?xml version="1.0" encoding="UTF-8"?>
       <placeholder/>
     </child>
     <child>
-      <object class="GtkFixed" id="myFixed">
-        <property name="name">wFixed</property>
-        <property name="width_request">587</property>
-        <property name="height_request">498</property>
+      <object class="GtkGrid" id="myGrid">
         <property name="visible">True</property>
         <property name="can_focus">False</property>
-        <property name="halign">baseline</property>
-        <property name="valign">baseline</property>
-        <child>
-          <object class="GtkButton" id="mySend">
-            <property name="label" translatable="yes">Send</property>
-            <property name="width_request">100</property>
-            <property name="visible">True</property>
-            <property name="can_focus">True</property>
-            <property name="receives_default">True</property>
-            <property name="halign">baseline</property>
-            <property name="valign">baseline</property>
-            <property name="resize_mode">immediate</property>
-            <signal name="clicked" handler="btnSend" swapped="no"/>
-          </object>
-          <packing>
-            <property name="x">5</property>
-            <property name="y">460</property>
-          </packing>
-        </child>
-        <child>
-          <object class="GtkButton" id="myMin">
-            <property name="label" translatable="yes">Minamize</property>
-            <property name="width_request">100</property>
-            <property name="visible">True</property>
-            <property name="can_focus">True</property>
-            <property name="receives_default">True</property>
-            <property name="halign">baseline</property>
-            <property name="valign">baseline</property>
-            <signal name="clicked" handler="btnMinamize" swapped="no"/>
-          </object>
-          <packing>
-            <property name="x">111</property>
-            <property name="y">460</property>
-          </packing>
-        </child>
-        <child>
-          <object class="GtkTreeView" id="myText">
-            <property name="width_request">100</property>
-            <property name="height_request">100</property>
-            <property name="visible">True</property>
-            <property name="can_focus">True</property>
-            <property name="halign">baseline</property>
-            <property name="valign">baseline</property>
-            <property name="margin_bottom">352</property>
-            <property name="enable_grid_lines">both</property>
-            <child internal-child="selection">
-              <object class="GtkTreeSelection"/>
-            </child>
-          </object>
-        </child>
+        <property name="hexpand">True</property>
+        <property name="vexpand">True</property>
         <child>
           <object class="GtkEntry" id="myEntry">
-            <property name="width_request">100</property>
+            <property name="width_request">600</property>
             <property name="height_request">80</property>
             <property name="visible">True</property>
             <property name="can_focus">True</property>
-            <property name="halign">baseline</property>
+            <property name="can_default">True</property>
+            <property name="has_default">True</property>
             <property name="valign">baseline</property>
+            <property name="hexpand">True</property>
             <property name="max_length">250</property>
+            <property name="activates_default">True</property>
             <property name="width_chars">30</property>
             <property name="max_width_chars">40</property>
             <property name="secondary_icon_name">face-smile-symbolic</property>
             <property name="secondary_icon_tooltip_text" translatable="yes">Insert Emoji</property>
             <property name="secondary_icon_tooltip_markup" translatable="yes">Insert Emoji</property>
+            <property name="placeholder_text" translatable="yes">Enter your Message Here:</property>
             <property name="show_emoji_icon">True</property>
             <property name="enable_emoji_completion">True</property>
+            <signal name="key-press-event" handler="key_pressed" swapped="no"/>
           </object>
           <packing>
-            <property name="x">273</property>
+            <property name="left_attach">0</property>
+            <property name="top_attach">0</property>
+          </packing>
+        </child>
+        <child>
+          <object class="GtkScrolledWindow" id="myScroll">
+            <property name="width_request">600</property>
+            <property name="height_request">400</property>
+            <property name="visible">True</property>
+            <property name="can_focus">True</property>
+            <property name="valign">baseline</property>
+            <property name="hexpand">True</property>
+            <property name="vexpand">True</property>
+            <property name="vscrollbar_policy">always</property>
+            <property name="shadow_type">in</property>
+            <property name="min_content_width">600</property>
+            <property name="min_content_height">400</property>
+            <property name="max_content_width">600</property>
+            <property name="max_content_height">400</property>
+            <property name="propagate_natural_width">True</property>
+            <property name="propagate_natural_height">True</property>
+            <child>
+              <object class="GtkTreeView" id="myText">
+                <property name="width_request">600</property>
+                <property name="height_request">400</property>
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <property name="valign">baseline</property>
+                <property name="hexpand">True</property>
+                <property name="vexpand">True</property>
+                <property name="enable_grid_lines">both</property>
+                <child internal-child="selection">
+                  <object class="GtkTreeSelection"/>
+                </child>
+              </object>
+            </child>
+          </object>
+          <packing>
+            <property name="left_attach">0</property>
+            <property name="top_attach">1</property>
+          </packing>
+        </child>
+        <child>
+          <object class="GtkFixed" id="myFixed">
+            <property name="name">wFixed</property>
+            <property name="width_request">600</property>
+            <property name="height_request">32</property>
+            <property name="visible">True</property>
+            <property name="can_focus">False</property>
+            <property name="valign">baseline</property>
+            <child>
+              <object class="GtkButton" id="mySend">
+                <property name="label" translatable="yes">Send</property>
+                <property name="width_request">100</property>
+                <property name="height_request">32</property>
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <property name="receives_default">True</property>
+                <property name="valign">baseline</property>
+                <property name="resize_mode">immediate</property>
+                <signal name="clicked" handler="btnSend" swapped="no"/>
+              </object>
+            </child>
+            <child>
+              <object class="GtkButton" id="myMin">
+                <property name="label" translatable="yes">Minamize</property>
+                <property name="width_request">100</property>
+                <property name="height_request">32</property>
+                <property name="sensitive">False</property>
+                <property name="can_focus">False</property>
+                <property name="receives_default">False</property>
+                <property name="valign">baseline</property>
+                <property name="image_position">right</property>
+                <signal name="clicked" handler="btnMinamize" swapped="no"/>
+              </object>
+              <packing>
+                <property name="x">500</property>
+              </packing>
+            </child>
+          </object>
+          <packing>
+            <property name="left_attach">0</property>
+            <property name="top_attach">2</property>
           </packing>
         </child>
       </object>
@@ -141,6 +175,7 @@ type GtkUserInterface struct {
 // you just place them in a map that names the signals, then feed the map to the builder
 var signals = map[string]interface{}{
 	// "btnConnect":  btnConnect,
+	"key_pressed": txtPressed,
 	"btnSend":     btnSend,
 	"btnMinamize": btnMin,
 }
@@ -204,6 +239,15 @@ func addRow(listStore *gtk.ListStore, username string, message string) {
 }
 
 // looks like handlers can literally be any function or method
+func txtPressed(win *gtk.Entry, event *gdk.Event) {
+	keyEvent := gdk.EventKeyNewFromEvent(event)
+	keyVal := keyEvent.KeyVal()
+	// Enter key will Send Text
+	if keyVal == 65421 || keyVal == 65293 {
+		btnSend()
+	}
+}
+
 func btnConnect() {
 	fmt.Println("Trying to connect...")
 }
@@ -213,6 +257,10 @@ func btnSend() {
 	s, e := data.GetText()
 	if e != nil {
 		log.Fatalln("Couldn't get Text:", e)
+	}
+	isEmptyCheck := strings.TrimSpace(s)
+	if isEmptyCheck == "" {
+		return
 	}
 	if is_connected {
 		chat_connection.Write([]byte(s + "\n"))
@@ -273,6 +321,17 @@ func check_chat() {
 				addRow(userInterface.myList, realMsg[0], realMsg[1])
 				userInterface.window.Show()
 				userInterface.window.GetFocus()
+				/*
+					scroll_obj, scroll_bad := userInterface.builder.GetObject("myScroll")
+					if scroll_bad != nil {
+						log.Fatalln("Couldn't get myScroll")
+					}
+					myScroll := scroll_obj.(*gtk.ScrolledWindow)
+
+					adj := myScroll.GetVAdjustment()
+					myNewSize := adj.GetUpper() - adj.GetPageSize()
+					adj.SetValue(myNewSize)
+				*/
 			}
 		}()
 	}
